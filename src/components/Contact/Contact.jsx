d165e7b522e0e6d4f5716f4581ae57a3ae618e50
julia <julia.zhyvylo@asila.mk>
2025-02-12 20:15:06 +0100

    delete contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,8 @@ import { ImUser } from "react-icons/im";
 
 import { contact_card, icon, button } from "./Contact.module.css";
 
-function Contact({ id, name, number }) {
+function Contact({ id, name, number, deleteContact }) {
+ 
   return (
     <div className={contact_card}>
       <div>
@@ -17,7 +18,7 @@ function Contact({ id, name, number }) {
         </p>
       </div>
       <div>
-        <button className={button} type="button">Delete</button>
+        <button className={button} type="button" onClick={() => deleteContact(id)}>Delete</button>
       </div>
     </div>
   );
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,12 @@
 import Contact from "../Contact/Contact";
 import { contacts_list } from "./ContactList.module.css";
 
-function ContactList({ contacts }) {
+function ContactList({ contacts, deleteContact }) {
   return (
     <ul className={contacts_list}>
       {contacts.map(({ id, name, number }) => (
         <li key={id}>
-          <Contact name={name} number={number} id={id}/>
+          <Contact name={name} number={number} id={id} deleteContact={deleteContact}/>
         </li>
       ))}
     </ul>
